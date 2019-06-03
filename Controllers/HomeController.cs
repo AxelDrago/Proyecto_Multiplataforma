c75b2106dba0c8dfe78d5a2ff9c2b4e3b97b03a3
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-03 17:54:05 -0500

    Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,25 @@ namespace Proyecto_Multiplataforma.Controllers
 {
     public class HomeController: Controller
     {
-        
-        public IActionResult
+         public IActionResult RegistroMascota()
+        {
+            ViewBag.Tipos = _context.Tipos.ToList();//lo nuevo en este proyecto
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistroMascota(Mascota m)
+        {
+            if (ModelState.IsValid){
+                _context.Add(m);
+                _context.SaveChanges();
+
+                return RedirectToAction("index");
+            }
+            ViewBag.Tipos = _context.Tipos.ToList();//lo nuevo
+
+            return View();
+        }
     }
 }
--- Models/Mascota.cs
+++ Models/Mascota.cs
@@ -2,6 +2,11 @@ namespace Proyecto_Multiplataforma.Models
 {
     public class Mascota
     {
-        
+        public int Id { get; set; }
+        public string NombreTemporal { get; set; }
+        public int Edad { get; set; }
+        public TipoMascota Tipo { get; set; }
+        public string Foto { get; set; }
+        public int TipoId { get; set; }
     }
 }
